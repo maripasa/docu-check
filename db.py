ec979894ab98ae34dc3e1b53edd85facaecaf0bc
asa <main.grudge573@aleeas.com>
2024-07-18 11:50:50 -0300

    Revert "Revert "refactor: making it look like gep""
    This reverts commit 6e66bff1a8ece5c7bdf82f8ee37f6f2aca4ad589.

---
+++ db.py
@@ -0,0 +1,11 @@
+import sqlalchemy
+import datetime as dt
+from sqlalchemy.orm import (
+    sessionmaker, 
+    declarative_base,
+)
+
+db = sqlalchemy
+engine = db.create_engine("postgresql+psycopg2://postgres:postgres@localhost/docucheck")
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
---
+++ models/consultation.py
@@ -0,0 +1,13 @@
+from db import db, Base
+
+class Consultation(Base):
+    __tablename__ = "consultations"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    consultation_date = db.Column(db.Date, nullable=False)
+    id_company = db.Column(db.Integer, db.ForeignKey("companies.id"), nullable=False)
+    
+    company = db.relationship("Company", back_populates="consultations")
+    
+    def __repr__(self):
+        return f"<Consultation(id={self.id}, consultation_date={self.consultation_date}, id_company={self.id_company})>"
